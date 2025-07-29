79d26fbf212a1e3458de60649e835bfefd6573ee
laxman singh <laxmansingh790616@gmail.com>
2025-07-29 06:17:28 +0530

    setup Vite + React + Tailwind + shadcn/ui and integrate Clerk auth (Google, GitHub, Microsoft)

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Navigate, Outlet } from 'react-router-dom'
+import { useUser } from '@clerk/clerk-react'
+
+function App() {
+  const {user, isLoaded, isSignedIn} =useUser();
+
+  if(!isSignedIn && isLoaded){
+    return <Navigate to={'/auth/sign-in'} />
+  }
+  return (
+    <>
+      <Outlet/>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,42 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import './index.css'
+import App from './App.jsx'
+import SignInPage from './auth/sign-in/index.jsx'
+import Home from './home/index.jsx'
+import Dashboard from './dashboard/index.jsx'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
+
+const router = createBrowserRouter([
+  {
+    element: <App/>,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/dashboard',
+        element: <Dashboard />
+      }
+    ]
+  },
+  {
+    path: '/auth/sign-in',
+    element: <SignInPage/>
+  }
+]);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <RouterProvider router={router}/>
+    </ClerkProvider>
+  </StrictMode>,
+)
