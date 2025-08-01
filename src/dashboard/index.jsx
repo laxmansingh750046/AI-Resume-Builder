e1df2129a28a1d868df68ee33093791fbb32b5c5
laxman singh <laxmansingh790616@gmail.com>
2025-08-01 19:33:07 +0530

    get user resumes and show them

--- src/dashboard/index.jsx
+++ src/dashboard/index.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect ,useState} from 'react'
 import AddResume from './components/AddResume.jsx'
+import { useUser } from '@clerk/clerk-react'
+import API from '../services/API.js';
+import ResumeCardItem from './components/ResumeCardItem.jsx';
 function Dashboard() {
+  const [resumeList, setResumeList]=useState([]);
+  const {user} = useUser();
+  
+  useEffect(()=>{
+    user&&GetResumesList();
+  },[user])
+
+  const GetResumesList = ()=>{
+    API.getUserResumes(user?.primaryEmailAddress?.emailAddress)
+    .then(res=>{
+      console.log(res.data);
+      setResumeList(res.data);
+    })
+  }
   return (
     <div className='p-10 md:px-20 lg:px-32'>
         <h2 className='font-bold text-3xl'>My Resume</h2>
@@ -9,6 +26,9 @@ function Dashboard() {
           mt-10 
         '>
           <AddResume />
+          {resumeList.length>0 && resumeList.map((resume, index)=>(
+            <ResumeCardItem resume={resume} key={resume.resumeId}/>
+          ))}
         </div>
     </div>
   )
