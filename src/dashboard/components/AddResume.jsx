41177a2ad25acd1f54bf82b4c358961ec58e333e
laxman singh <laxmansingh790616@gmail.com>
2025-08-01 19:33:27 +0530

    addresume

--- src/dashboard/components/AddResume.jsx
+++ src/dashboard/components/AddResume.jsx
@@ -1,4 +1,4 @@
-import { PlusSquare } from "lucide-react";
+import { Loader2, PlusSquare} from "lucide-react";
 import {Button} from "../../components/ui/button.jsx"
 import {Input} from "../../components/ui/input.jsx"
 import {
@@ -7,18 +7,39 @@ import {
   DialogDescription,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
+} from "../../components/ui/dialog.jsx";
 import { useState } from "react";
 import {v4 as uuidv4} from 'uuid'
+import { useUser } from "@clerk/clerk-react";
+import API from '../../services/API.js'
+import { useNavigate } from "react-router-dom";
 
 function AddResume() {
   const [openDialog, setOpenDialog] = useState(false);
   const [resumeTitle, setResumeTitle] = useState("");
+  const [loading, setLoading] = useState(false);
+  const {user} = useUser();
+  const navigation = useNavigate();
 
   const onCreate = ()=>{
+    setLoading(true);
     const uuid = uuidv4();
-    console.log(resumeTitle," ",uuid)
+    const data = {
+      data: {
+        title: resumeTitle,
+        resumeId: uuid,
+        userEmail: user?.primaryEmailAddress?.emailAddress,
+        userName: user?.fullName
+      }
+    }
+    API.createResume(data)
+      .then(res=>{
+        console.log(res)
+        if(res){
+          navigation('/dashboard/resume/'+uuid+'/edit')
+        }
+      })
+      .finally(setLoading(false))
   }
   return (
     <div>
@@ -44,8 +65,12 @@ function AddResume() {
             <div className="flex justify-end gap-4">
               <Button variant="ghost" onClick={()=>setOpenDialog(false)}>Cancel</Button>
               <Button 
-                disabled={!resumeTitle} 
-                onClick={onCreate}>Create</Button>
+                disabled={!resumeTitle || loading} 
+                onClick={onCreate}>
+                  {loading
+                  ? <Loader2 className="animate-spin" />
+                  : "Create"}
+                </Button>
             </div>
           </DialogHeader>
         </DialogContent>
