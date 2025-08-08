38d5ab174fdfbda33fd192eb8eee3d9b10773007
laxman singh <laxmansingh790616@gmail.com>
2025-08-08 22:37:37 +0530

    debug preview components

--- client/src/dashboard/resume/components/preview/SkillsPreview.jsx
+++ client/src/dashboard/resume/components/preview/SkillsPreview.jsx
@@ -13,7 +13,7 @@ function SkillsPreview({resumeInfo}) {
     }} />
 
     <div className='grid grid-cols-2 gap-x-10 gap-3 my-4'>
-        {resumeInfo?.skills.map((skill)=>(
+        {resumeInfo?.skills?.map((skill)=>(
             <div key={skill?.id} className='flex items-center justify-between'>
                 <h2 className='text-xs'>{skill.name}</h2>
                 <div className='h-2 bg-gray-200 w-[120px] ml-3'>
