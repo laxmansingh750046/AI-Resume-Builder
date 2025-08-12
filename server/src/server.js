9a91376cacf6ced338a7a77b4d2c1d496acf2d50
laxman singh <laxmansingh790616@gmail.com>
2025-08-12 13:59:44 +0530

    finishing touch

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,26 @@
-import 'dotenv/config'
+import 'dotenv/config';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import connectDB from './db/index.js';
-import { app } from './app.js'
-    
+import { app } from './app.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 connectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 8000,()=>{
+.then(() => {
+    const frontendPath = path.join(__dirname, '../client/dist'); // for Vite
+
+    app.use(express.static(frontendPath));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(frontendPath, 'index.html'));
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
         console.log(`server is running at port ${process.env.PORT || 8000}`);
     });
 })
-.catch((error)=>{
-    console.log('mongodb connection failed ',error);
-})      
+.catch((error) => {
+    console.log('mongodb connection failed', error);
+});
