99eb1765e1e56785b3cffb8c189ad1ad8dd006d9
laxman singh <laxmansingh790616@gmail.com>
2025-08-13 07:59:30 +0530

    debug

--- server/src/server.js
+++ server/src/server.js
@@ -1,22 +1,9 @@
 import 'dotenv/config';
-import path from 'path';
-import { fileURLToPath } from 'url';
 import connectDB from './db/index.js';
 import { app } from './app.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 connectDB()
 .then(() => {
-    const frontendPath = path.join(__dirname, '../client/dist'); // for Vite
-
-    app.use(express.static(frontendPath));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(frontendPath, 'index.html'));
-    });
-
     app.listen(process.env.PORT || 8000, () => {
         console.log(`server is running at port ${process.env.PORT || 8000}`);
     });
