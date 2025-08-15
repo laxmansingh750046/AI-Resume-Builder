4ee0c810e75712737dc5d8f57cc6e632215253fa
laxman singh <laxmansingh790616@gmail.com>
2025-08-15 10:27:22 +0530

    refactor: remove temporary notification about AI resume generation unavailability from Header component

--- client/src/components/custom/Header.jsx
+++ client/src/components/custom/Header.jsx
@@ -4,12 +4,6 @@ import { Link } from "react-router-dom";
 function Header() {
   const { isSignedIn, user } = useUser();
   return (
-    <div>
-      <div className="bg-red-600 text-white px-4 py-2 text-center font-medium">
-        🚧 AI Resume Generation Temporarily Unavailable — We’re upgrading our
-        system for better security. You can still create resumes manually.
-      </div>
-
       <div className="p-3 px-5 flex justify-between shadow-md">
         <img src="/logo.svg" width={60} height={50} />
 
@@ -26,7 +20,6 @@ function Header() {
           </Link>
         )}
       </div>
-    </div>
   );
 }
 
